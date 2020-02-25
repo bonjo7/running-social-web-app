42bc696f30b13342dfe6f33059127e562ff3f52d
Bernard Thompson <bethomps@redhat.com>
2020-02-25 16:52:11 +0000

    Updated nav to be responsive

--- src/components/views/NavBar.js
+++ src/components/views/NavBar.js
@@ -2,36 +2,26 @@ import React from "react";
 import {
   Navbar,
   Nav,
-  NavDropdown,
-  Form,
-  Button,
-  FormControl
+  Form
 } from "react-bootstrap";
 import "./NavBar.css";
 import { Link} from 'react-router-dom'
 
 const NavBar = () => {
   return (
-    // <nav class="navbar bg-dark">
-    //   <h4>
-    //     <a href="index.html"> Running Social</a>
-    //   </h4>
-    //   <ul>
-    //     <li><a href="members.html">Members</a></li>
-    //     <li><a href="events.html">Events</a></li>
-    //     <li><a href="login.html">Login</a></li>
-    //   </ul>
-    // </nav>
-    <Navbar bg='dark' variant='dark'>
+    <Navbar collapseOnSelect expand="lg" bg='dark' variant='dark'>
       <Navbar.Brand>
       <Link to='/'> Running Social <i class="fas fa-running"></i></Link>
       </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
       <Nav className='mr-auto'></Nav>
       <Form inline>
         <Link to='/members'>Members</Link>
         <Link to='/events'>Events</Link>
         <Link to='/login'>Login </Link>
       </Form>
+      </Navbar.Collapse>
     </Navbar>
   );
 };
