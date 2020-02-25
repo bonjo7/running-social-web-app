78318bb906c6ad815ec8fc46c3c07bb24a471811
Bernard Thompson <bethomps@redhat.com>
2020-02-25 16:22:23 +0000

    Created register layout

--- src/components/views/Register.js
+++ src/components/views/Register.js
@@ -1,10 +1,40 @@
-import React from 'react'
+import React, {Fragment} from 'react';
+import { Link } from 'react-router-dom';
+import './Register.css';
 
 const Register = () => {
     return (
-        <div>
-            <h2>Register</h2>
+        <Fragment>
+      <h2 className="large create-account"><i class="fas fa-user-circle"></i> Create Your Account</h2>
+      <form className="form" action="create-profile.html">
+        <div className="form-group">
+          <input type="text" placeholder="Name" name="name" required />
         </div>
+        <div className="form-group">
+          <input type="email" placeholder="Email Address" name="email" />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            minLength="6"
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            name="password2"
+            minLength="6"
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Register" />
+      </form>
+      <p className="my-1">
+        Already have an account? <Link to="/login">Sign In</Link>
+      </p>
+      </Fragment>
     )
 }
 
