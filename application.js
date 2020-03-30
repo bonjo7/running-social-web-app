77462decdaa952c752e15274e2a68b532c7c2be5
Bernard Thompson <bethomps@redhat.com>
2020-03-30 10:49:53 +0100

    Added application.js to allow running on openshift

---
+++ application.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+const port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+const host = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+const server = app.listen(port, host, () => {
+  console.log(`App started at: ${new Date()} on port: ${port}`);
+});
+module.exports = server;
